cb6f40c6c8c3094b7fe03495330cf018fe63bf7a
Prakash Kelkar <pkelkar@yahoo.com>
2022-05-29 20:29:13 +0530

    New Controller Added
    Added HomeController

---
+++ GCPTestWebAPI/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GCPTestWebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetServerTime()
+        {
+            string currDatetime = "Current UTC Date Time is:" + DateTime.UtcNow.ToLongDateString();
+            return Ok(currDatetime);
+        }
+    }
+}
